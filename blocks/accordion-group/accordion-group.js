a9128dad465adf6e190bfff1e1dd8c979977230e
Smit Shah <smit.shah@teknopoint.in>
2024-11-13 17:26:13 +0530

    accordion block created

---
+++ blocks/accordion-group/accordion-group.js
@@ -0,0 +1,53 @@
+import { fetchPlaceholders } from '../../scripts/aem.js';
+import { generateAccordionDOM } from '../accordion/accordion.js';
+
+export default async function decorate(block) {
+  const resp = await fetchPlaceholders();
+  // console.log("placeholder resp :: ", resp);
+  // each row is an accordion entry
+  const accordions = [...block.children];
+
+  // loop through all accordion blocks
+  [...accordions].forEach((accordion) => {
+    // generate the accordion
+    const accordionDOM = generateAccordionDOM(accordion);
+    // empty the content ,keep root element with UE instrumentation
+    accordion.textContent = '';
+    // add block classes
+    accordion.classList.add('accordion', 'block');
+    accordion.append(accordionDOM);
+
+  });
+
+  // use same styling as shade-box from /docs
+  block.classList.add('shade-box');
+  try {
+    openFunctionFAQ(block);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function openFunctionFAQ(block) {
+  const titles = block.querySelectorAll('details summary');
+
+  titles.forEach((title) => {
+    title.addEventListener('click', function () {
+      if (this.classList.contains('active')) {
+        setTimeout(() => {
+          this.closest('details').removeAttribute('open');
+        });
+        this.classList.remove('active');
+      } else {
+        titles.forEach((title) => {
+          title.closest('details').removeAttribute('open');
+          title.classList.remove('active');
+        });
+        this.classList.toggle('active');
+      }
+    });
+  });
+}
+
+
+
